d9571ccda057b74de6012fbfdf751027dff18167
TaHaSyedKhan <exploit.any@gmail.com>
2022-06-30 14:44:37 +0530

    some base css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
