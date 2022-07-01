f6d7e8bc18e2ebd7224fa131f7f0ac150fac5565
TaHaSyedKhan <exploit.any@gmail.com>
2022-07-01 17:33:19 +0530

    Fetched data from api and displays as cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,39 @@
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
 
-const Card = () => {
+    const Card = ({ data: { confirmed, recovered, deaths, lastUpdate}}) => {
+        console.log(recovered);
+    if(!confirmed){
+        return 'loading...'
+    }
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.card}>
+            <div className={styles.infected}>
+                <h3>Infected</h3>
+                <h5>
+                    <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+                </h5>
+                <h5>{new Date(lastUpdate).toDateString()}</h5>
+                <p>Number of active cases of covid-19</p>
+            </div>
+            <div className={styles.recovered}>
+                <h3>Recovered</h3>
+                <h5>
+                     <CountUp start={0} end={recovered.value} duration={2.5} separator="," />  
+                </h5>
+                <h5>{new Date(lastUpdate).toDateString()}</h5>
+                <p>Number of recoveries from covid-19</p>
+            </div>
+            <div className={styles.deaths}>
+                <h3>Deaths</h3>
+                <h5>
+                     <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+                </h5>
+                <h5>{new Date(lastUpdate).toDateString()}</h5>
+                <p>Number of deaths caused by covid-19</p>
+            </div>
+        </div>
     );
 }
 
